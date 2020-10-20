1760836a627c77460b98807ca318e82edf7bb4cc
Moises Huaringa <moises.huaringa@tektonlabs.com>
2020-10-20 14:15:42 -0500

    refactory footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
+import { AppBar, Link, Toolbar, Typography } from '@material-ui/core'
 import Navigation from '../../components/Navigation'
 import { useStyles } from './useStyles'
 
 const Footer = () => {
   const classes = useStyles()
+  const date = new Date()
   return (
     <AppBar className={classes.root} color="transparent" elevation={0}>
       <Toolbar>
         <Typography variant="body2" noWrap style={{ flexGrow: 1 }}>
-          Footer &copy; 2020
+          &copy; {date.getFullYear()} {process.env.REACT_APP_NAME} is powered by 
+          &nbsp;
+          <Link
+            href={process.env.REACT_APP_GITHUB}
+            target="_blank"
+          >
+            {process.env.REACT_APP_SIGNATURE} 
+          </Link>
         </Typography>
-
         <Navigation />
       </Toolbar>
     </AppBar>
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,30 +1,47 @@
 import React from 'react'
-import { Button, Link, Typography } from '@material-ui/core'
+import { Button, Divider, Icon } from '@material-ui/core'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    icon: {
+      marginRight: theme.spacing(2)
+    },
+    divider: {
+      margin: theme.spacing(3, 2)
+    }
+  })
+)
+const items = [
+  'Only Logo',
+  'Logo + Isotype - H',
+  'Logo + Isotype - V',
+  'Only Isotype'
+]
 const Navigation = () => {
+  const classes = useStyles()
   const [active, setActive] = React.useState<string>('Only Logo') 
-  const handleClick = () => {
-    console.info('handleClick')
-  }
-  const items = [
-    'Only Logo',
-    'Logo + Isotype (H)',
-    'Logo + Isotype (V)',
-    'Only Isotype'
-  ]
   return (
     <React.Fragment>
-      {items.map((item) => (
-        <Button 
-          key={item}
-          onClick={handleClick}
-          color={item !== active ? 'inherit' : 'primary'}
-          // style={{ fontWeight: item === active ? 'bold' : 'normal'}}
-          disabled={item === active}
-          size="small"
-        >
-          {item}
-        </Button>
+      <Icon className={classes.icon}>videogame_asset</Icon>
+      {items.map((item, key) => (
+        <React.Fragment>
+          <Button 
+            key={item}
+            onClick={() => console.info(item)}
+            disabled={item === active}
+            size="small"
+          >
+            {item}
+          </Button>
+          {items.length !== key + 1 && (
+            <Divider 
+              key={item} 
+              orientation="vertical" 
+              flexItem className={classes.divider}
+            />
+          )}
+        </React.Fragment>
       ))}
     </React.Fragment>  
   )
