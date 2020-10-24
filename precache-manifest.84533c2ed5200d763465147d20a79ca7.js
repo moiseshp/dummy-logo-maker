d18a6bcec5adff642c0c6a630c40e87eba266724
Moises Huaringa <moises.huaringa@tektonlabs.com>
2020-10-24 01:50:37 -0500

    Updates

---
+++ precache-manifest.84533c2ed5200d763465147d20a79ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cf47a127a79c12cb9dba3e6ea5d1b1",
+    "url": "/dummy-logo-maker/index.html"
+  },
+  {
+    "revision": "6f3eb4d3bb3fb93d65c9",
+    "url": "/dummy-logo-maker/static/js/2.929813fd.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/dummy-logo-maker/static/js/2.929813fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb92df0bdb19097e68ce",
+    "url": "/dummy-logo-maker/static/js/main.eda13966.chunk.js"
+  },
+  {
+    "revision": "ef3b97a40e1996e23de7",
+    "url": "/dummy-logo-maker/static/js/runtime-main.e8cf880e.js"
+  }
+]);
