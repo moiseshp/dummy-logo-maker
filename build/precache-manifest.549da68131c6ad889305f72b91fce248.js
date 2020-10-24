c216e2b4b993d0ee841ebb70183b8d087279045c
Moises Huaringa <moises.huaringa@tektonlabs.com>
2020-10-24 00:07:56 -0500

    build page

---
+++ build/precache-manifest.549da68131c6ad889305f72b91fce248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f82fb075dd31d2c503b25274f19ccb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7cf70dc73e835b42a0f",
+    "url": "/static/js/2.f10c17c7.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.f10c17c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4adc6cf73d0519f60c",
+    "url": "/static/js/main.fe70f622.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
